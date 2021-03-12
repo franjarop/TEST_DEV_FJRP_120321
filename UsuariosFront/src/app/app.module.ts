08ca0cf459f68bab124dc8e316b83a83328f221c
franjarop <franjarop.sistemas@gmail.com>
2021-03-12 12:11:12 -0600

    Se agrego funcion para exportar a excel

--- UsuariosFront/src/app/app.module.ts
+++ UsuariosFront/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import {NgxPaginationModule} from 'ngx-pagination'
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
--- UsuariosFront/src/app/components/usuarios/reporte-usuario/reporte-usuario.component.ts
+++ UsuariosFront/src/app/components/usuarios/reporte-usuario/reporte-usuario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ExcelService } from 'src/app/services/excel.service';
 import { ReportService } from 'src/app/services/report.service';
+import * as XLSX from 'xlsx'
 
 @Component({
   selector: 'app-reporte-usuario',
@@ -8,10 +10,21 @@ import { ReportService } from 'src/app/services/report.service';
 })
 export class ReporteUsuarioComponent implements OnInit {
 
-  constructor(public reportService:ReportService) { }
+  constructor(public reportService:ReportService, public exportExcelService:ExcelService) { }
 
   ngOnInit(): void {
     this.reportService.obtenerReporteUsuarios();
+  }
+  fileNameExcel="ReporteUsuarios.xlsx";
+
+  exportexcel():void{
+    let element = document.getElementById("excel-table");
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb,ws,"Sheet1");
+
+    XLSX.writeFile(wb, this.fileNameExcel);
   }
 
 }
